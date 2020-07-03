cb11fabc1a58920f1ce6d016325b84b730ea5b6e
CodingwithJess <thejessiebess@gmail.com>
2020-07-02 19:03:49 -0600

    copied instructions over to README for easier access. Started rough pseudocoding for JS and added basic starting HTML for timeblocks

---
+++ Assets/script.js
@@ -0,0 +1,8 @@
+// daily planner to create schedule
+// when I open planner, the current day is displayed at the top of the calendar
+// when I scroll down, I am presented with timeblocks for standard business hours (8-5)= 10 timeblocks
+// each timeblock is color coded to indicate whether it is in the past, present, or future
+// CLICK into timeblock button, I can enter an event (text) and CLICK save button
+// timeblock text is saved into LOCAL STORAGE
+// timeblock text stays even when page is refreshed
+
