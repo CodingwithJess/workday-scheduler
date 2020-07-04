4048bba622ee571107a6a695edc02dc1246d697a
CodingwithJess <thejessiebess@gmail.com>
2020-07-04 11:02:24 -0600

    added some basic CSS styling. Added moment.js formula to show current date at the top of page

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,11 @@
 // when I open planner, (document ready?)
 
 // the current day is displayed at the top of the calendar (moment.js?)
+const m = moment();
 
+console.log(m.format('MMMM Do YYYY'));
+
+$("#currentDay").text(m.format("MMMM Do YYYY"))
 
 
 //I am presented with timeblocks for standard business hours (8-5)= 10 timeblocks
@@ -10,5 +14,8 @@
 // each timeblock is color coded to indicate whether it is in the past, present, or future
 // CLICK into timeblock button, I can enter an event (text) and CLICK save button
 // timeblock text is saved into LOCAL STORAGE
+var words;
+var hourInfo;
+
 // timeblock text stays even when page is refreshed
 
