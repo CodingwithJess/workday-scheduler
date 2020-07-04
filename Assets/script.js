4546f40ed8f6948b344992052c55cb3c2e957da9
CodingwithJess <thejessiebess@gmail.com>
2020-07-04 10:00:31 -0600

    added time block rows in HTML and updated pseudocoding notes in JS file

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,12 @@
-// daily planner to create schedule
-// when I open planner, the current day is displayed at the top of the calendar
-// when I scroll down, I am presented with timeblocks for standard business hours (8-5)= 10 timeblocks
+// when I open planner, (document ready?)
+
+// the current day is displayed at the top of the calendar (moment.js?)
+
+
+
+//I am presented with timeblocks for standard business hours (8-5)= 10 timeblocks
+
+
 // each timeblock is color coded to indicate whether it is in the past, present, or future
 // CLICK into timeblock button, I can enter an event (text) and CLICK save button
 // timeblock text is saved into LOCAL STORAGE
