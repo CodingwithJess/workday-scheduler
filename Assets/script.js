5aa4990064ed2bc6c85acc0b286825c1053f1a0c
CodingwithJess <thejessiebess@gmail.com>
2020-07-06 16:12:36 -0600

    added for loop to add hours on schedule in HTML dynamically for workday

--- Assets/script.js
+++ Assets/script.js
@@ -13,3 +13,27 @@ function clock (){
 }
 
 setInterval(clock, 1000);
+
+
+// adding textareas to HTML for workday slots
+
+for (var hour = 8; hour < 17; hour++) {
+  scheduledHours.push(moment({hour}).format('h  a'));
+  $('.container').append(`<div class='row time-block' data-time='${hour}'>
+       <!--hour column-->
+           <div class='col-sm col-md-2 hour'>
+             <p>${moment({hour}).format('h  a')}</p>
+           </div>
+        <!--scheduling column-->
+           <div class='col-sm col-md-10 d-flex description'>
+              <div class='input-group'>
+                <textarea class="form-control text-area"></textarea>
+                <div class='input-group-append'>
+                  <button class='save-button d-flex justify-center align-center'>
+                    <i class='far fa-save fa-2x save-icon'></i>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>`);
+}
