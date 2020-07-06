09ac2efdad700a865a67ea7acbeb524d3a667b68
CodingwithJess <thejessiebess@gmail.com>
2020-07-06 16:03:15 -0600

    started JS from scratch to incorporate more jQuery functionality to add HTML elements to page. Added clock function to top of page

--- Assets/script.js
+++ Assets/script.js
@@ -1,64 +1,15 @@
-// when I open planner, (document ready?)
-var words;
-var hourInfo;
-// the current day is displayed at the top of the calendar (moment.js?)
-const m = moment();
+// global variables
+var scheduledHours = [];
+var availableHours = {};
+var m = moment ();
+var newDay = moment().hours(0);
+var currentTime = m.hour();
 
-console.log(m.format("MMMM Do YYYY"));
+// pushing time clock and date to currentDay id in HTML
 
-$("#currentDay").text(m.format("MMMM Do YYYY"));
-
-// each timeblock is color coded to indicate whether it is in the past, present, or future
-
-$(document).ready(function () {
-  colorChange();
-  // renderText ();
-});
-
-function colorChange() {
-  var realTime = moment().hours();
-  var timeTest = parseInt($(this).attr("id"));
-  if (realTime > timeTest) {
-    $(this).removeClass("future");
-    $(this).removeClass("present");
-    $(this).addClass("past");
-  } else if (realTime < timeTest) {
-    $(this).removeClass("present");
-    $(this).removeClass("past");
-    $(this).addClass("future");
-  } else {
-    $(this).removeClass("future");
-    $(this).removeClass("past");
-    $(this).addClass("present");
-  }
+function clock (){
+  var dateString = moment().format("MMMM Do YYYY, h:mm a");
+  $("#currentDay").html(dateString);
 }
 
-// CLICK into timeblock button, I can enter an event (text) and CLICK save button
-// timeblock text is saved into LOCAL STORAGE
-
-$(".saveBtn").click(function () {
-  words = $(this).siblings(".input").text();
-  console.log(words);
-  hourInfo = $(this).siblings(".hour").text();
-  console.log(hourInfo);
-  localStorage.setItem(hourInfo, JSON.stringify(words));
-});
-
-// timeblock text stays even when page is refreshed
-
-function getScheduleFromLS() {
-  var schedule;
-  if (localStorage.getItem("schedule") === null) {
-    schedule = [];
-  } else {
-    schedule = JSON.parse(localStorage.getItem("schedule"));
-  }
-  $.each(schedule, function (index, item) {
-    $("#" + item.time)
-      .children(".description")
-      .append(item.plan);
-    if (item.plan !== "") {
-      clearPlan($("#" + item.time).children(".hour"));
-    }
-  });
-}
+setInterval(clock, 1000);
